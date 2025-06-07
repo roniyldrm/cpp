7d839fa3d36c3519df98929e02e5fb473195d899
ergenbalik <roniyildirim872@gmail.com>
2025-06-07 17:21:13 +0300

    implementation of optional continues

--- mySTLImplementations/headers/my_std_optional.hpp
+++ mySTLImplementations/headers/my_std_optional.hpp
@@ -13,35 +13,51 @@ class my_optional{
         //constructers
         my_optional() = delete;
         
+        my_optional(val_type&& o) : has_value(true){
+            new (&buffer[0]) val_type{ std:forward<val_type>(o)};
+        }
+
         template<typename...Types>
         my_optional(Types&&...args) : has_value(true){
             new (&buffer[0]) val_type{ std::forward<Types>(args)...};
         }
 
-        my_optional(val_type&& o) : has_value(true){
-            new (&buffer[0]) val_type{ std:forward<val_type>(o)};
-        }
 
         //~my_optional(){}
         
     public:
-
+        
         //operators
         constexpr bool operator bool(){ return has_value;}
         
-        constexpr val_type&& operator*() const{ 
+        constexpr val_type& operator*() &{ 
             if (!this->has_value){
                 throw std::logic_error("")
             } 
-            return static_cast<val_type&&>(buffer[0]);
+            return *std::launder(reinterpret_cast<val_type&>(buffer[0]));
+            //? i used launder to safely inform compiler that a new object was constructed via placement new 
+            //? returns pointer to the same type you give it,
+            //? but with the guarantee that the compiler knows a new object was created at that memory address.
+            
         }
 
-        constexpr val_type* operator->() const{
+        constexpr val_type&& operator*() && {}
+        constexpr const val_type& operator*() const &{}
+        constexpr const val_type&& operator*() const &&{}
+        //TODO implement all operators for each optional type lv ,rv, const lv, const rv
+
+        constexpr val_type* operator->() &{
             if (!this->has_value){
                 throw std::logic_error("")
             }
-            return static_cast<val_type*>(&buffer[0]);
+            return std::launder(reinterpret_cast<val_type*>(&buffer[0]));
         }
+
+        constexpr val_type* operator->() &&{}
+        constexpr const val_type* operator->() const &{}
+        constexpr const val_type* operator->() const &&{}
+
+
 
         constexpr my_optional& operator=(val_type&& o) const{
             if(!this->has_value){
@@ -49,22 +65,20 @@ class my_optional{
                                                                     //should be changed via swap() ???
             }
         }
-//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
         template<typename...Types>
         constexpr my_optional& operator=(Types&&...Args) const{
             if(!this->has_value){
-                new (&buffer[0]) val_type{ std:forward<val_type>(o)};//i thought it as 1 time initialized only 
-                                                                    //should be changed via swap() ???
+                new (&buffer[0]) val_type{ std::forward<Types>(Args)...};
             }
         }
 
 
-        //functions
-        constexpr _construct
 
+        //functions
         template<typename...Types>
         constexpr val_type& emplace(Types&&...Args) const{
-            
+            new (&buffer[0]) val_type{ std::forward<Types>(Args)...};
         }
 
         constexpr bool has_value() const{ return has_value;}
@@ -78,11 +92,5 @@ class my_optional{
         constexpr my_optional& swap(val_type&& o){
             //?? dont know what it could be used for 
         }
-
-
-
-
-
-
 
 };
--- mySTLImplementations/my_std_optional.cpp
+++ mySTLImplementations/my_std_optional.cpp
@@ -1,1 +1,18 @@
-#include "./headers/my_std_optional.hpp"
+#include <iostream>
+#include <optional>
+#include <type_traits>
+
+int main() {
+    int a = 5;
+    std::optional<int> o1{a};
+    std::optional<int> o2{5};
+
+    auto& r1 = *o1;
+    auto& r2 = *o2;
+
+    std::cout << std::boolalpha;
+    std::cout << std::is_lvalue_reference_v<decltype(*o1)> << "\n";          // true
+    std::cout << std::is_lvalue_reference_v<decltype(*o2)> << "\n";          // true
+
+    std::cout << std::is_rvalue_reference_v<decltype(*std::move(o1))> << "\n"; // true
+}
