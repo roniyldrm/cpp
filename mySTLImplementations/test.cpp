f313dca8689d55d211b188eb6a8a0ecdafc37e9a
ergenbalik <roniyildirim872@gmail.com>
2025-06-10 17:47:28 +0300

    test

--- mySTLImplementations/test.cpp
+++ mySTLImplementations/test.cpp
@@ -13,20 +13,36 @@ int main(){
     }
 
     auto it = arr.begin();
-
     std::cout<<*it;
-
+    //////////////////////////
     my_optional<std::pair<int,std::string>> opt;
-    
     my_optional<std::pair<int,std::string>> opt2 = {1990,"feyyaz yigit"};
 
     std::cout << opt.value().first;
     std::cout << opt.value().second;
 
-    opt = {1000,"abcde"};
+    opt = std::pair<int,std::string>{1000,"abcde"};
+    opt = {2000,"aaa"};
 
-    opt2.emplace(opt);
+    opt2.emplace(1500,"asdasda");
     
     std::cout << opt2.value().first;
     std::cout << opt2.value().second;
+
+    opt2.swap(opt);
+
+    std::cout << opt2.value().first;
+    std::cout << opt2.value().second;
+
+    auto fun = [](bool e) -> my_optional<std::string> {
+        if(e){
+            return my_optional<std::string>{"str"};
+        }
+        return my_nullopt;
+    };
+
+    std::string str = *fun(false);
+    std::string str2 = *fun(true);
+    
+    std::cout << str << str2;
 }
